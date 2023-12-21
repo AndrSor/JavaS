5859a5d828447d809f4cc72c4c885d792924f772
Сороколат Андрей <AESorokolat@sberbank.ru>
2023-12-21 19:30:35 +0300

    Lesson 1

--- A01.java
+++ A01.java
@@ -2,18 +2,9 @@ public class A01 {
     // Home Work Lesson1
     public static void main(String[] args) {
 
-        System.out.println("Home Work Lesson1");
+        System.out.println("Hello");
 
-        Child ch1 = new Child();
-        Child ch2 = new Child("Артем");
-        Child.stPrintParent(1);
-        Child.stPrintParent(2);
-        Child.stPrintChildren(1);
-        Child.stPrintChildren(2);
-        ch2.nonStPrintParent(1);
-        ch2.nonStPrintParent(2);
-        ch2.nonStPrintChildren(1);
-        ch2.nonStPrintChildren(2);
+
 
     }
 
